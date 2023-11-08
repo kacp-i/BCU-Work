e1bb508cd956fa9dafc3574027ac897de93989da
illuzionkek <132145999+illuzionkek@users.noreply.github.com>
2023-11-08 08:41:05 +0000

    Create L8library.py

---
+++ L8library.py
@@ -0,0 +1,15 @@
+class BankAccount():
+    accountReg = {}
+    
+    
+    def getAccount():
+        pass
+    def getBalance():
+        pass
+    def deposit():
+        pass
+    def withdraw():
+        pass
+    def transfer():
+        pass
+    
