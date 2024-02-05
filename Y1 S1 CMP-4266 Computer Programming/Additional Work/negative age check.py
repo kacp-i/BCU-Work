c3d2192d0686b62a1642856303aaf349bb05e4df
_k <132145999+illuzionkek@users.noreply.github.com>
2024-02-05 10:26:23 +0000

    Add files via upload

---
+++ Y1 S1 CMP-4266 Computer Programming/Additional Work/negative age check.py
@@ -0,0 +1,6 @@
+while True:
+    userAge = int(input("What is your age?\n"))
+    if userAge < 0:
+        print("Age cant be negative")
+    else:
+        break
---
+++ Y1 S1 CMP-4266 Computer Programming/Additional Work/odd and even numbers.py
@@ -0,0 +1,7 @@
+print("All even numbers from 0 - 20:")
+for i in range (2, 21, 2):
+    print(i)
+
+print("\nAll odd numbers from 0 - 20:")    
+for i in range (1, 21, 2):
+    print(i)
