cefa975d1213c123ec36f041e31ea462462e3bd2
_k <132145999+kacp-i@users.noreply.github.com>
2024-05-15 09:20:14 +0100

    Add files via upload

---
+++ Y1 S2 CMP-4285 - Innovation Project/serial communication.py
@@ -0,0 +1,36 @@
+# https://roboticsbackend.com/raspberry-pi-arduino-serial-communication/
+
+# Code for Arduino Side
+""" 
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Hello from Arduino!");
+  delay(1000);
+}
+"""
+
+# Code for Raspberry Pi Side
+import serial
+
+# 1st param = device name
+# 2nd param = baud rate (bps)
+# 3rd param = timeout
+serialCon = serial.Serial("", 9600, timeout=1)
+
+serialCon.reset_input_buffer() # wipes the input incase of any excess data
+
+# Infinite loop to read data coming in
+while True:
+    if serialCon.in_waiting > 0:
+        line = serialCon.readline().decode('utf-8').rstrip()
+        print(line)
+
+# Infinite loop to send data
+while True:
+        serialCon.write(b"Hello from Raspberry Pi!\n")
+        line = serialCon.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(1)
