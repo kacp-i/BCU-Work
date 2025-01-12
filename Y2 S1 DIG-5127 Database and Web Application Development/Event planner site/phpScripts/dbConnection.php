2008955234a0b6283d84129c275678c87963c88e
._k <132145999+kacp-i@users.noreply.github.com>
2025-01-12 09:54:37 +0000

    Add files via upload

---
+++ Y2 S1 DIG-5127 Database and Web Application Development/Event planner site/phpScripts/dbConnection.php
@@ -0,0 +1,17 @@
+<?php
+    // setting out variables to connect to database
+    $host= "localhost";
+    $username= "root";
+    $password = "";
+    $dbName = "eventplanningsite";
+
+    // variables used in connecting to database: databasehost, username, password, databasename, port, socket
+    $conn = mysqli_connect($host, $username, $password, $dbName);
+    
+    // checking the connection
+    if (!$conn) {
+        echo "Connection failed!";
+    }
+    else {
+        echo "Connection found!";
+    }
---
+++ Y2 S1 DIG-5127 Database and Web Application Development/Event planner site/phpScripts/dbLogin.php
@@ -0,0 +1,72 @@
+<?php
+    // starts a session
+    session_start();
+
+    // grabs the connection file
+    include "dbConnection.php";
+
+    // checks if the inputs are NULL
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+
+        // function to validate data / remove "noise"
+        function validate($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+        // validating the username and password values
+        $username = validate($_POST['username']);
+        $password = validate($_POST['password']);
+
+        // if the username is empty, give an error
+        if (empty($username)) {
+            header("Location: ../index.php?error=User Name is required");
+            exit();
+        }
+        // if the password is empty, give an error
+        else if(empty($password)) {
+            header("Location: ../index.php?error=Password is required");
+            exit();
+        }
+        else {
+            // creating the sql query
+            $sql = "SELECT * FROM customerdetails WHERE username='$username' AND password='$password'";
+
+            // performing the query and storing the results as a variable
+            $result = mysqli_query($conn, $sql);
+
+            // checking if the amount of rows results to 1
+            if (mysqli_num_rows($result) === 1) {
+
+                // stores the row as an associative array (key, value) as a variable
+                $row = mysqli_fetch_assoc($result);
+
+                // checking if the username and password values in the row match the user input
+                if ($row['username'] === $username && $row['password'] === $password) {
+                    // sends a message to confirm a successful login
+                    echo "Logged in!";
+
+                    // create a session variable
+                    $_SESSION['username'] = $row['username'];
+                    $_SESSION['id'] = $row['id'];
+                    
+                    header("Location: ../index.php");
+                    exit();
+                }
+                else {
+                    header("Location: ../index.php?error=Incorect User name or password");
+                    exit();
+                }
+            }
+            else {
+                header("Location: ../index.php?error=Incorect User name or password");
+                exit();
+            }
+        }
+    }
+    else {
+        header("Location: ../index.php");
+        exit();
+    }
