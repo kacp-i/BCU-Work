88850241fbca7658af9186995c7dd0c2e5634ee9
._k <132145999+kacp-i@users.noreply.github.com>
2025-01-23 10:37:04 +0000

    Update dbLogin.php

--- Y2 S1 DIG-5127 Database and Web Application Development/Event planner site/phpScripts/dbLogin.php
+++ Y2 S1 DIG-5127 Database and Web Application Development/Event planner site/phpScripts/dbLogin.php
@@ -5,10 +5,10 @@
     // grabs the connection file
     include "dbConnection.php";
 
-    // checks if the inputs are NULL
+    // checks if the inputs arent NULL
     if (isset($_POST['username']) && isset($_POST['password'])) {
 
-        // function to validate data / remove "noise"
+        // function to remove "noise"
         function validate($data) {
             $data = trim($data);
             $data = stripslashes($data);
@@ -16,7 +16,7 @@
             return $data;
         }
 
-        // validating the username and password values
+        // grabbing all the values and making them usable
         $username = validate($_POST['username']);
         $password = validate($_POST['password']);
 
@@ -31,6 +31,7 @@
             exit();
         }
         else {
+            // trying the customer table first
             // creating the sql query
             $sql = "SELECT * FROM customerdetails WHERE username='$username' AND password='$password'";
 
@@ -51,19 +52,57 @@
                     // create a session variable
                     $_SESSION['username'] = $row['username'];
                     $_SESSION['id'] = $row['id'];
-                    $_SESSION['type'] = 0;
+                    $_SESSION['accType'] = "customer";
                     
+                    // redirect
                     header("Location: ../index.php");
                     exit();
                 }
                 else {
-                    header("Location: ../index.php?error=Incorect User name or password");
+                    // redirect to login page with an error
+                    header("Location: ../login.php?error=Incorect User name or password");
                     exit();
                 }
             }
             else {
-                header("Location: ../index.php?error=Incorect User name or password");
-                exit();
+                // trying the business table
+                // creating the sql query
+                $sql = "SELECT * FROM businessdetails WHERE username='$username' AND password='$password'";
+
+                // performing the query and storing the results as a variable
+                $result = mysqli_query($conn, $sql);
+
+                // checking if the amount of rows results to 1
+                if (mysqli_num_rows($result) === 1) {
+
+                    // stores the row as an associative array (key, value) as a variable
+                    $row = mysqli_fetch_assoc($result);
+
+                    // checking if the username and password values in the row match the user input
+                    if ($row['username'] === $username && $row['password'] === $password) {
+                        // sends a message to confirm a successful login
+                        echo "Logged in!";
+
+                        // create a session variable
+                        $_SESSION['username'] = $row['username'];
+                        $_SESSION['id'] = $row['id'];
+                        $_SESSION['accType'] = "business";
+                        
+                        // redirect
+                        header("Location: ../index.php");
+                        exit();
+                    }
+                    else {
+                        // redirect to login page with an error
+                        header("Location: ../login.php?error=Incorect User name or password");
+                        exit();
+                    }
+                }
+                else {
+                    // redirect to login page with an error
+                    header("Location: ../login.php?error=Incorect User name or password");
+                    exit();
+                }
             }
         }
     }
