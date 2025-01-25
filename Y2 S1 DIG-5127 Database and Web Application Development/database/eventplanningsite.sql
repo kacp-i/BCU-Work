08b5c2cffe7b6f9ce92119ff0a56ace959f550ba
._k <132145999+kacp-i@users.noreply.github.com>
2025-01-25 21:09:37 +0000

    Add files via upload

--- Y2 S1 DIG-5127 Database and Web Application Development/database/eventplanningsite.sql
+++ Y2 S1 DIG-5127 Database and Web Application Development/database/eventplanningsite.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2025 at 06:10 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Jan 25, 2025 at 10:08 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,11 +29,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `businesses` (
   `businessID` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
-  `phone` int(11) NOT NULL,
-  `email` int(11) NOT NULL,
-  `type` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `username` varchar(25) NOT NULL,
+  `password` char(255) NOT NULL,
+  `email` char(255) NOT NULL,
+  `phoneNum` varchar(15) NOT NULL,
+  `addressLineOne` char(255) NOT NULL,
+  `postCode` char(25) NOT NULL,
+  `firstName` char(50) NOT NULL,
+  `lastName` char(50) NOT NULL,
+  `regDate` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `businesses`
+--
+
+INSERT INTO `businesses` (`businessID`, `username`, `password`, `email`, `phoneNum`, `addressLineOne`, `postCode`, `firstName`, `lastName`, `regDate`) VALUES
+(1, 'buss', 'buss', 'buss@gmail.com', '+441210121111', '91 buss lane', 'BU33SS', 'buss', 'ssub', '2025-01-24 16:50:32');
 
 -- --------------------------------------------------------
 
@@ -70,10 +82,17 @@ INSERT INTO `customerdetails` (`customerID`, `username`, `password`, `email`, `p
 CREATE TABLE `eventdetails` (
   `eventID` int(11) NOT NULL,
   `locationID` int(11) NOT NULL,
-  `service_id` int(11) NOT NULL,
-  `customerID` int(11) NOT NULL,
-  `eventType` text NOT NULL
+  `serviceID` int(11) NOT NULL,
+  `customerID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `eventdetails`
+--
+
+INSERT INTO `eventdetails` (`eventID`, `locationID`, `serviceID`, `customerID`) VALUES
+(3, 2, 6, 1),
+(4, 2, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -87,6 +106,15 @@ CREATE TABLE `locationdetails` (
   `address` text NOT NULL,
   `size` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `locationdetails`
+--
+
+INSERT INTO `locationdetails` (`locationID`, `name`, `address`, `size`) VALUES
+(1, 'Merrydale Manor', 'Knutsford, Chesire', 2000),
+(2, 'Farnham Castle', 'Farnham, Surrey', 875),
+(3, 'Bredenbury Court Barns', 'Hereford, Herefordshire', 1200);
 
 -- --------------------------------------------------------
 
@@ -106,31 +134,29 @@ CREATE TABLE `reviewdetails` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `servicedetails`
+-- Table structure for table `services`
 --
 
-CREATE TABLE `servicedetails` (
-  `service_id` int(11) NOT NULL,
-  `username` varchar(25) DEFAULT NULL,
-  `password` text NOT NULL,
-  `serviceType` text NOT NULL,
-  `serviceWebsite` text NOT NULL,
-  `serviceHQ` text NOT NULL,
-  `serviceEmail` text NOT NULL,
-  `servicePhone` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci COMMENT='will need to create a location list for services as an inter';
-
--- --------------------------------------------------------
+CREATE TABLE `services` (
+  `serviceID` int(11) NOT NULL,
+  `serviceName` char(50) NOT NULL,
+  `serviceType` char(50) NOT NULL,
+  `serviceProvider` char(50) NOT NULL,
+  `serviceDescription` text NOT NULL,
+  `servicePPD` decimal(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Table structure for table `servicelocations`
+-- Dumping data for table `services`
 --
 
-CREATE TABLE `servicelocations` (
-  `servicelocationID` int(11) NOT NULL,
-  `locationID` int(11) NOT NULL,
-  `service_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci COMMENT='relationship between services and locations provided';
+INSERT INTO `services` (`serviceID`, `serviceName`, `serviceType`, `serviceProvider`, `serviceDescription`, `servicePPD`) VALUES
+(1, 'Child Entertainment', 'Entertainment', 'Kids R Us', 'Kids R Us specializes in providing engaging and interactive experiences for children aged 2 to 12 years. Our mission is to create magical moments that inspire creativity, joy, and learning through various fun-filled activities.', 299.99),
+(2, 'Hamburger Cheeseburger BigMac Whopper', 'Catering', 'McDonalds', 'McDonald\'s is a globally recognized fast-food chain that serves a diverse range of affordable and delicious meals, making it a popular choice for families, individuals, and children. Known for its iconic golden arches, McDonald\'s aims to provide an enjoyable dining experience with a focus on quality, convenience, and customer satisfaction.', 799.99),
+(3, 'Photobomb', 'Photography', 'Intel', 'Photobomb is a professional photography company dedicated to preserving life’s most cherished memories through stunning imagery. Specializing in a wide range of photography services, we aim to provide clients with high-quality photos that tell their unique stories.', 1250.99),
+(4, 'Feastingables', 'Catering', 'Mr Burger', 'This innovative fast-food concept leverages delivery-only services to reach fans and customers nationwide. The brand is known for its delicious burgers, fries, and unique menu items, all created with a focus on quality and customer satisfaction.', 19.00),
+(5, 'Elegant Events Catering', 'Catering', 'EEC', 'Elegant Events Catering is a premier wedding catering company dedicated to creating unforgettable culinary experiences for couples on their special day. With a focus on high-quality ingredients, exceptional service, and customized menus, we aim to elevate wedding celebrations with delicious food that reflects the couple’s unique tastes and style.', 2000.00),
+(6, 'Freddy Fez', 'Entertainment', 'FF Inc', 'Freddy Fez is an enchanting traveling circus that brings joy and wonder to audiences of all ages. With a rich tradition of performing arts, our circus combines breathtaking acrobatics, whimsical clowns, and mesmerizing animal acts to create a magical experience that captivates and entertains.', 840.75);
 
 -- --------------------------------------------------------
 
@@ -168,7 +194,7 @@ ALTER TABLE `customerdetails`
 ALTER TABLE `eventdetails`
   ADD PRIMARY KEY (`eventID`),
   ADD KEY `locationID` (`locationID`),
-  ADD KEY `service_id` (`service_id`),
+  ADD KEY `service_id` (`serviceID`),
   ADD KEY `customerID` (`customerID`);
 
 --
@@ -186,19 +212,10 @@ ALTER TABLE `reviewdetails`
   ADD KEY `customerID` (`customerID`);
 
 --
--- Indexes for table `servicedetails`
+-- Indexes for table `services`
 --
-ALTER TABLE `servicedetails`
-  ADD PRIMARY KEY (`service_id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- Indexes for table `servicelocations`
---
-ALTER TABLE `servicelocations`
-  ADD PRIMARY KEY (`servicelocationID`),
-  ADD KEY `locationID` (`locationID`),
-  ADD KEY `service_id` (`service_id`);
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`serviceID`);
 
 --
 -- Indexes for table `staffdetails`
@@ -213,10 +230,34 @@ ALTER TABLE `staffdetails`
 --
 
 --
+-- AUTO_INCREMENT for table `businesses`
+--
+ALTER TABLE `businesses`
+  MODIFY `businessID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `customerdetails`
 --
 ALTER TABLE `customerdetails`
   MODIFY `customerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `eventdetails`
+--
+ALTER TABLE `eventdetails`
+  MODIFY `eventID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `locationdetails`
+--
+ALTER TABLE `locationdetails`
+  MODIFY `locationID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `serviceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -227,7 +268,7 @@ ALTER TABLE `customerdetails`
 --
 ALTER TABLE `eventdetails`
   ADD CONSTRAINT `eventdetails_ibfk_1` FOREIGN KEY (`locationID`) REFERENCES `locationdetails` (`locationID`),
-  ADD CONSTRAINT `eventdetails_ibfk_2` FOREIGN KEY (`service_id`) REFERENCES `servicedetails` (`service_id`),
+  ADD CONSTRAINT `eventdetails_ibfk_2` FOREIGN KEY (`serviceID`) REFERENCES `services` (`serviceID`),
   ADD CONSTRAINT `eventdetails_ibfk_3` FOREIGN KEY (`customerID`) REFERENCES `customerdetails` (`customerID`);
 
 --
@@ -236,13 +277,6 @@ ALTER TABLE `eventdetails`
 ALTER TABLE `reviewdetails`
   ADD CONSTRAINT `reviewdetails_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `servicedetails` (`service_id`),
   ADD CONSTRAINT `reviewdetails_ibfk_2` FOREIGN KEY (`customerID`) REFERENCES `customerdetails` (`customerID`);
-
---
--- Constraints for table `servicelocations`
---
-ALTER TABLE `servicelocations`
-  ADD CONSTRAINT `servicelocations_ibfk_1` FOREIGN KEY (`locationID`) REFERENCES `locationdetails` (`locationID`),
-  ADD CONSTRAINT `servicelocations_ibfk_2` FOREIGN KEY (`service_id`) REFERENCES `servicedetails` (`service_id`);
 
 --
 -- Constraints for table `staffdetails`
